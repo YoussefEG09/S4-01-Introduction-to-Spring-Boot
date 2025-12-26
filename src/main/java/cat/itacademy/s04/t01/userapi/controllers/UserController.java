b814bbf10196e300d281aa124381f9504cd5e88f
YoussefEG09 <youssef.eg09@gmail.com>
2025-12-26 13:07:59 +0100

    feat: add UserController with in-memory user list

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -0,0 +1,15 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import cat.itacademy.s04.t01.userapi.models.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private static final List<User> users = new ArrayList<>();
+}
